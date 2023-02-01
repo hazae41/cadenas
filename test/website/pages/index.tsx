0bf29da651a2ccf18d43353c7a18077b160a4dd3
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-02-01 11:58:27 +0100

    test

--- test/website/pages/index.tsx
+++ test/website/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
 
       await tls.handshake()
 
-      const headers = { "Content-Type": "application/json" }
+      const headers = new Headers({ "Content-Type": "application/json" })
       const body = JSON.stringify({ "jsonrpc": "2.0", "method": "web3_clientVersion", "params": [], "id": 67 })
       const res = await fetch("https://eth.llamarpc.com", { stream: tls, method: "POST", headers, body })
 
