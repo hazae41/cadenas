3a273140cdfd5af7c146e1947540d4888b5f1bb2
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-03-04 11:15:32 +0100

    client

--- src/mods/index.ts
+++ src/mods/index.ts
@@ -1,6 +1,6 @@
 export * from "./algorithms/index.js"
 export * from "./binary/index.js"
 export * from "./ciphers/index.js"
+export * from "./client.js"
 export * from "./extensions.js"
-export * from "./tls.js"
 
